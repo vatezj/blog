87f9e7fd6b5b9dd07b097ce2126eebcd91e8649e
vate <2324757255@qq.com>
2017-11-06 14:41:31 +0800

    vate

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,6 +5,7 @@
                 <div class="col-sm-12">
                     <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }}</a></span> |
                     <span>粤ICP备16067937号-1</span>
+                    <a href="https://m.kuaidi100.com/" target="_blank">快递查询</a>
                 </div>
             </div>
         </div>
