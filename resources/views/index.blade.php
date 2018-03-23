9372854dbde31a359af9de9fb9819dbc478fd7a4
vate <2324757255@qq.com>
2018-03-23 08:53:32 +0800

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,7 @@
         <p class="links">
             <font aria-hidden="true">»</font>
             <a href="{{ route('post.index') }}" aria-label="点击查看博客文章列表">博客</a>
+            <a href="about" aria-label="点击查看博客文章列表">关于</a>
             @foreach($pages as $page)
                 <font aria-hidden="true">/</font><a href="{{ route('page.show',$page->name) }}"
                                                     aria-label="查看{{ $author or 'author' }}的{{ $page->display_name }}">{{$page->display_name }}</a>
